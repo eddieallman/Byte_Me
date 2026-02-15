1cfceea5b0b8528e6ea91500bfedbb062ec826f6
marcos ashton <marcosashiglesias@gmail.com>
2026-02-15 22:08:57 +0000

    fixed accessibility page

--- frontend/src/app/(public)/accessibility/page.tsx
+++ frontend/src/app/(public)/accessibility/page.tsx
@@ -10,51 +10,51 @@ export default function CookiePage() {
 
       <div className="border rounded-lg shadow-inner h-[70vh] overflow-y-scroll p-6 bg-white">
         <div className="prose prose-lg max-w-none space-y-4">
-          <h2>Accessibility Statement for <span class="basic-information website-name">ByteMe</span></h2>
+          <h2>Accessibility Statement for <span className="basic-information website-name">ByteMe</span></h2>
           <p>
-            This is an accessibility statement from <span class="basic-information organization-name">ByteMe</span>.
+            This is an accessibility statement from <span className="basic-information organization-name">ByteMe</span>.
           </p>
           <h3>Measures to support accessibility</h3>
           <p>
-            <span class="basic-information organization-name">ByteMe</span>
+            <span className="basic-information organization-name">ByteMe</span>
             takes the following measures to ensure accessibility of
-            <span class="basic-information website-name">ByteMe</span>:
+            <span className="basic-information website-name">ByteMe</span>:
           </p>
-          <ul class="organizational-effort accessibility-measures">
+          <ul className="organizational-effort accessibility-measures">
             <li>Include accessibility throughout our internal policies.</li>
             <li>Integrate accessibility into our procurement practices.</li>
           </ul>
           <h3>Conformance status</h3>
           <p>
             The <a href="https://www.w3.org/WAI/standards-guidelines/wcag/">Web Content Accessibility Guidelines (WCAG)</a> defines requirements for designers and developers to improve accessibility for people with disabilities. It defines three levels of conformance: Level A, Level AA, and Level AAA.
-            <span class="basic-information website-name">ByteMe</span>
+            <span className="basic-information website-name">ByteMe</span>
             is
-            <span class="basic-information conformance-status" data-printfilter="lowercase">partially conformant</span>
+            <span className="basic-information conformance-status" data-printfilter="lowercase">partially conformant</span>
             with
-            <span class="basic-information conformance-standard"><span data-negate="">WCAG 2.2 level AA</span>.</span>
+            <span className="basic-information conformance-standard"><span data-negate="">WCAG 2.2 level AA</span>.</span>
             <span>
-            <span class="basic-information conformance-status">Partially conformant</span>
+            <span className="basic-information conformance-status">Partially conformant</span>
             means that
-            <span class="basic-information conformance-meaning">some parts of the content do not fully conform to the accessibility standard</span>.
+            <span className="basic-information conformance-meaning">some parts of the content do not fully conform to the accessibility standard</span>.
           </span>
           </p>
           <h3>Feedback</h3>
           <p>
             We welcome your feedback on the accessibility of
-            <span class="basic-information website-name">ByteMe</span>.
+            <span className="basic-information website-name">ByteMe</span>.
             Please let us know if you encounter accessibility barriers on
-            <span class="basic-information website-name">ByteMe</span>:
+            <span className="basic-information website-name">ByteMe</span>:
           </p>
-          <ul class="basic-information feedback h-card">
-            <li class="contact-other p-note">At our website: <a href="https://frontend-production-c253c.up.railway.app/support">https://frontend-production-c253c.up.railway.app/support</a></li>
+          <ul className="basic-information feedback h-card">
+            <li className="contact-other p-note">At our website: <a href="https://frontend-production-c253c.up.railway.app/support">https://frontend-production-c253c.up.railway.app/support</a></li>
           </ul>
           <h3>Technical specifications</h3>
           <p>
             Accessibility of
-            <span class="basic-information website-name">ByteMe</span>
+            <span className="basic-information website-name">ByteMe</span>
             relies on the following technologies to work with the particular combination of web browser and any assistive technologies or plugins installed on your computer:
           </p>
-          <ul class="technical-information technologies-used">
+          <ul className="technical-information technologies-used">
             <li>HTML</li>
             <li>CSS</li>
             <li>TypeScript</li>
@@ -63,27 +63,27 @@ export default function CookiePage() {
           <h3>Limitations and alternatives</h3>
           <p>
             Despite our best efforts to ensure accessibility of
-            <span class="basic-information website-name">ByteMe</span> , there may be some limitations. Below is a description of known limitations, and potential solutions. Please contact us if you observe an issue not listed below.
+            <span className="basic-information website-name">ByteMe</span> , there may be some limitations. Below is a description of known limitations, and potential solutions. Please contact us if you observe an issue not listed below.
           </p>
           <p>
             Known limitations for
-            <span class="basic-information website-name">ByteMe</span>:
+            <span className="basic-information website-name">ByteMe</span>:
           </p>
-          <ol class="technical-information accessibility-limitations">
+          <ol className="technical-information accessibility-limitations">
             <li><strong>All pages</strong>: Text to speech page reader not implemented because It is not included in this iteration of the website. We plan to implement in the next iteration. .</li>
           </ol>
           <h3>Assessment approach</h3>
           <p>
-            <span class="basic-information organization-name">ByteMe</span>
+            <span className="basic-information organization-name">ByteMe</span>
             assessed the accessibility of
-            <span class="basic-information website-name">ByteMe</span>
+            <span className="basic-information website-name">ByteMe</span>
             by the following approaches:
           </p>
-          <ul class="technical-information assessment-approaches">
+          <ul className="technical-information assessment-approaches">
             <li>Self-evaluation</li>
           </ul>
           <h3>Formal complaints</h3>
-          <p class="complaints">We plan to implement issue forums in the next iteration of this website</p>
+          <p className="complaints">We plan to implement issue forums in the next iteration of this website</p>
         </div>
       </div>
     </main>
